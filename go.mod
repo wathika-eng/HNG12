012310a9cbb996105f13a25fcba8b3803c76a6be
wathika-eng <wathika02@gmail.com>
2025-01-29 19:23:50 +0300

    Remove unused dependencies and clean up code structure by deleting unnecessary files and functions

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module simpleAPI
 
 go 1.23.5
-
-require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,40 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/joho/godotenv"
+	"time"
 )
 
-const baseURL = "https://api.github.com/users"
 const PORT = ":8000"
 
 // run the API when called
 func main() {
-	// load dotenv file
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("Error loading .env file")
-	}
 	// mux controls traffic
 	mux := http.NewServeMux()
-
-	mux.HandleFunc("GET /user/{username}", getUser)
 	mux.HandleFunc("GET /", getUser)
-	// delete logs
-	defer os.Remove("logs.json")
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt, syscall.SIGINT)
+
 	// create a server
 	log.Printf("Listening on http://localhost%v/\n", PORT)
-	go func() {
-		http.ListenAndServe(PORT, mux)
-	}()
-	// when user stops abruptly, ensure logs are also deleted since it's a defer fn
-	<-stop
-	log.Printf("Shutting down server and deleting logs...\n")
+	http.ListenAndServe(PORT, mux)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	// ensure data is in json format
+	w.Header().Set("Content-Type", "application/json")
+	userData := map[string]interface{}{
+		"email":            "wathika02@gmail.com",
+		"current_datetime": time.Now().UTC().Format(time.RFC3339),
+		"github_url":       "https://github.com/wathika-eng/simple_github_user_api",
+	}
+
+	data, err := json.MarshalIndent(userData, "", "  ")
+	if err != nil {
+		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+		return
+	}
+	// 200, okay
+	w.WriteHeader(http.StatusOK)
+	// json data
+	w.Write(data)
 }
